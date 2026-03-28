53c0290797d1177ce599372116f8cbd626203dc8
r08b46009 <89798516+r08b46009@users.noreply.github.com>
2026-03-29 03:28:08 +0800

    Update Parser.cpp

--- HW4/src/Parser.cpp
+++ HW4/src/Parser.cpp
@@ -1,168 +1,150 @@
-// #include "Parser.hpp"
-#include <unordered_map>
-#include "Data.hpp"
+#include "Parser.hpp"
 
-#include <string>
-#include <vector>
 #include <fstream>
 #include <sstream>
-using namespace std;
-class Parser
-{
-    public:
-    string nodeFile, plFile, sclFile;
-    int maxDisplacement;
-    vector<Cell *> cells, terminals;
-    vector<Row *> rows;
+#include <stdexcept>
+#include <unordered_map>
 
-    void read(string const &filename);
-    Parser() : maxDisplacement(0) {}
-    LegalizerInput *parse(char argv[]);
-};
+using std::getline;
+using std::ifstream;
+using std::runtime_error;
+using std::stringstream;
+using std::string;
+using std::unordered_map;
 
-
-void Parser::read(string const &filename1)
-{
-    string i, buff, t;
+void Parser::read(const string& filename1) {
     ifstream fin(filename1);
-    unordered_map<string, Cell *> strToCell;
+    if (!fin) {
+        throw runtime_error("Failed to open aux file: " + filename1);
+    }
 
-    while (getline(fin, buff))
-    {
-        if (buff.empty())
+    string token, buff, t;
+    unordered_map<string, Cell*> strToCell;
+
+    while (getline(fin, buff)) {
+        if (buff.empty()) {
             continue;
-
-        stringstream input(buff);
-        input >> i;
-        if (i == "RowBasedPlacement")
-        {
-            input >> t >> this->nodeFile >> this->plFile >> this->sclFile;
         }
-        else if (i == "MaxDisplacement")
-        {
-            input >> t >> this->maxDisplacement;
+        stringstream input(buff);
+        input >> token;
+        if (token == "RowBasedPlacement") {
+            input >> t >> nodeFile >> plFile >> sclFile;
+        } else if (token == "MaxDisplacement") {
+            input >> t >> maxDisplacement;
         }
     }
 
+    const string baseDir = filename1.substr(0, filename1.find_last_of('/'));
 
-    string filename = filename1.substr(0, filename1.find_last_of('/'));
-    ifstream fin2(filename+ '/' + this->nodeFile);
-    int nodeNum = 0, terminalNum = 0;
-    while (getline(fin2, buff))
-    {
-        if (buff.empty())
+    ifstream fin2(baseDir + '/' + nodeFile);
+    if (!fin2) {
+        throw runtime_error("Failed to open nodes file: " + baseDir + '/' + nodeFile);
+    }
+
+    int nodeNum = 0;
+    int terminalNum = 0;
+    while (getline(fin2, buff)) {
+        if (buff.empty()) {
             continue;
-
-        stringstream buffer(buff);
-        
-        buffer >> i;
-        if (i == "NumNodes")
-        {
-            buffer >> t >> nodeNum;
         }
-        else if (i == "NumTerminals")
-        {
+        stringstream buffer(buff);
+        buffer >> token;
+        if (token == "NumNodes") {
+            buffer >> t >> nodeNum;
+        } else if (token == "NumTerminals") {
             buffer >> t >> terminalNum;
             break;
         }
     }
-    getline(fin2, buff);
-        string n;
-        int w = 0, h = 0;
-    int const cellNum = nodeNum - terminalNum;
-    for (int i = 0; i < nodeNum; ++i)
-    {
+
+    getline(fin2, buff);  // skip one line after header
+
+    const int cellNum = nodeNum - terminalNum;
+    for (int i = 0; i < nodeNum; ++i) {
         getline(fin2, buff);
         stringstream buffStream(buff);
 
-        buffStream >> n >> w >> h;
-        auto cell = new Cell(n, w, h);
-        if (i < cellNum)
-            this->cells.push_back(cell);
-        else
-            this->terminals.push_back(cell);
-        strToCell.emplace(n, cell);
+        string name;
+        int w = 0;
+        int h = 0;
+        buffStream >> name >> w >> h;
+        auto* cell = new Cell(name, w, h);
+        if (i < cellNum) {
+            cells.push_back(cell);
+        } else {
+            terminals.push_back(cell);
+        }
+        strToCell.emplace(name, cell);
     }
 
-   ifstream fin3(filename  + '/' + this->plFile);
-    int size = this->cells.size()+this->terminals.size();
+    ifstream fin3(baseDir + '/' + plFile);
+    if (!fin3) {
+        throw runtime_error("Failed to open placement file: " + baseDir + '/' + plFile);
+    }
 
-    for (int i = 0; i < size; ++i)
-    {
+    const int totalObjects = static_cast<int>(cells.size() + terminals.size());
+    for (int i = 0; i < totalObjects; ++i) {
         getline(fin3, buff);
         stringstream buffStream(buff);
         string name;
-        double x = 0, y = 0;
+        double x = 0;
+        double y = 0;
         buffStream >> name >> x >> y;
         strToCell.at(name)->x = x;
         strToCell.at(name)->y = y;
     }
-       ifstream fin4(filename + '/' + this->sclFile);
-    int rowNum = 0;
-    // string buff;
-    string id;
-    while (getline(fin4, buff))
-    {
-        if (buff.empty())
-            continue;
 
+    ifstream fin4(baseDir + '/' + sclFile);
+    if (!fin4) {
+        throw runtime_error("Failed to open row file: " + baseDir + '/' + sclFile);
+    }
+
+    int rowNum = 0;
+    while (getline(fin4, buff)) {
+        if (buff.empty()) {
+            continue;
+        }
         stringstream buffer(buff);
-        
-        buffer >> id;
-        if (id == "NumRows")
-        {
+        buffer >> token;
+        if (token == "NumRows") {
             buffer >> t >> rowNum;
             break;
         }
     }
-    for (int i = 0; i < rowNum; ++i)
-    {
-        int y = 0, h = 0, siteWidth = 0, x = 0, siteNum = 0;
-        while (getline(fin4, buff))
-        {
-            if (buff.empty())
+
+    for (int i = 0; i < rowNum; ++i) {
+        int y = 0;
+        int h = 0;
+        int siteWidth = 0;
+        int x = 0;
+        int siteNum = 0;
+        while (getline(fin4, buff)) {
+            if (buff.empty()) {
                 continue;
-
+            }
             stringstream buffer(buff);
-            
-            buffer >> id;
-            if (id == "Coordinate")
-            {
+            buffer >> token;
+            if (token == "Coordinate") {
                 buffer >> t >> y;
-            }
-            else if (id == "Height")
-            {
+            } else if (token == "Height") {
                 buffer >> t >> h;
-            }
-            else if (id == "Sitewidth")
-            {
+            } else if (token == "Sitewidth") {
                 buffer >> t >> siteWidth;
-            }
-            else if (id == "NumSites")
-            {
+            } else if (token == "NumSites") {
                 buffer >> t >> siteNum;
-            }
-            else if (id == "SubrowOrigin")
-            {
+            } else if (token == "SubrowOrigin") {
                 buffer >> t >> x;
-            }
-
-            else if (id == "End")
-            {
-                auto row = new Row(siteWidth, h, y);
+            } else if (token == "End") {
+                auto* row = new Row(siteWidth, h, y);
                 row->subRows.push_back(new SubRow(x, x + siteWidth * siteNum));
-                this->rows.push_back(row);
+                rows.push_back(row);
                 break;
             }
         }
     }
 }
 
-
-LegalizerInput *Parser::parse(char argv[])
-{
-    string auxFilePath(argv);
-    read(auxFilePath);
-
-    return new LegalizerInput(this->maxDisplacement, this->cells, this->terminals, this->rows);
+LegalizerInput* Parser::parse(const char* auxPath) {
+    read(string(auxPath));
+    return new LegalizerInput(maxDisplacement, cells, terminals, rows);
 }
