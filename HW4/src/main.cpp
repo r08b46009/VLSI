3fdaaa9184c2d751a41d6906742c822ee9a08a97
r08b46009 <89798516+r08b46009@users.noreply.github.com>
2026-03-29 03:30:04 +0800

    Update main.cpp

--- HW4/src/main.cpp
+++ HW4/src/main.cpp
@@ -1,50 +1,44 @@
-
-#include "abacus.hpp"
 #include "Parser.hpp"
-#include "Data.hpp"
-#include <iostream>
+#include "abacus.hpp"
+
 #include <ctime>
 #include <fstream>
-using namespace std;
+#include <iostream>
 
-
-
-int main(int argc, char **argv)
-{
-    clock_t loadTime1, loadTime2, ecTime1, ecTime2, outputTime1, outputTime2;
-    if (argc < 2)
-    {
-        cerr << "Usage: " << argv[0] << " <aux file>\n";
-        return 0;
+int main(int argc, char** argv) {
+    clock_t loadTime1, loadTime2, execTime1, execTime2, outputTime1, outputTime2;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <aux file> <output file>\n";
+        return 1;
     }
-    cout<< "1331"<<endl;
+
     loadTime1 = clock();
     Parser parser;
-    auto input = parser.parse(argv[1]);
+    auto* input = parser.parse(argv[1]);
     loadTime2 = clock();
-    
-    ecTime1 = clock();
-    abacus abacus(input);
 
-    ecTime2 = clock();
+    execTime1 = clock();
+    Abacus legalizer(input);
+    execTime2 = clock();
+
     outputTime1 = clock();
+    std::ofstream fout(argv[2]);
+    if (!fout) {
+        std::cerr << "Failed to open output file: " << argv[2] << "\n";
+        return 1;
+    }
 
-
-
-    cout<<"123"<<endl;
-    string auxFilePath(argv[1]);
-    ofstream fout(argv[2]);
-    for (auto const &cell : input->cells)
-        fout << cell->n << ' '<< (cell->optimalX) << ' '<< (cell->optimalY) << " : N\n";
-    for (auto const &terminal : input->terminals)
-        fout << terminal->n << ' '<< (terminal->x) << ' '<< (terminal->y) << " : N /FIXED\n";
-    
-    // result->write(argv);
+    for (const auto* cell : input->cells) {
+        fout << cell->n << ' ' << cell->optimalX << ' ' << cell->optimalY << " : N\n";
+    }
+    for (const auto* terminal : input->terminals) {
+        fout << terminal->n << ' ' << terminal->x << ' ' << terminal->y << " : N /FIXED\n";
+    }
     outputTime2 = clock();
-    cout<<"loading:" <<double(loadTime2-loadTime1)/CLOCKS_PER_SEC<<endl;
-    cout<<"abacus process:" <<double(ecTime2-ecTime1)/CLOCKS_PER_SEC<<endl;
-    cout<<"ouput:" <<double(outputTime2-outputTime1)/CLOCKS_PER_SEC<<endl;
 
+    std::cout << "loading: " << double(loadTime2 - loadTime1) / CLOCKS_PER_SEC << '\n';
+    std::cout << "abacus process: " << double(execTime2 - execTime1) / CLOCKS_PER_SEC << '\n';
+    std::cout << "output: " << double(outputTime2 - outputTime1) / CLOCKS_PER_SEC << '\n';
 
     return 0;
 }
